88b795b809f87a43f6b7598b51046f9be93f8874
Alano <alanosantosdev@gmail.com>
2022-06-03 20:33:53 -0300

    requisito 1 completo

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,9 @@
-const fetchProducts = (id) => {
+const fetchProducts = async (id) => {
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${id}`;
-
-  fetch(url)
-    .then((resposta) => resposta.json())
-    .then((data) => console.log(data));
+  if (id === undefined) { return Promise.reject(new Error('You must provide an url')); }
+  const resposta = await fetch(url);
+  const dados = await resposta.json();
+  return dados;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,5 +6,29 @@ describe('1 - Teste a função fetchProducts', () => {
     it('Testa se fetchProducts é uma função', () =>{
     expect(fetchProducts).toBeInstanceOf(Object);
   });
- /*  fail('Teste vazio'); */
+  it('Execute a função fetchProducts com o argumento computador e teste se fetch foi chamada;', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it('teste se, ao chamar a função fetchProducts com o argumento computador, a função fetch tiliza o endpoint https://api.mercadolibre.com/sites/MLB/search?q=computador', async () => {
+    const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(api);
+  });
+  it('Teste se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {
+    const product = await fetchProducts('computador');
+    expect(product).toEqual(computadorSearch);
+  });
+   it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem:You must provide an url', async () =>{
+    try {
+      await fetchProducts();
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toEqual(new Error('You must provide an url'));
+    }
+    });
+
+
+  
+ /*  fail('Teste vazio'); */ 
 });
