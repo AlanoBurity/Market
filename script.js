f9c82b3c2e6e4e4d86e91c47e754dd85f76e6f73
Alano <alanosantosdev@gmail.com>
2022-06-06 20:09:48 -0300

    requisito 2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -23,8 +23,18 @@ const createProductItemElement = ({ sku, name, image }) => {
 
   return section;
 };
+const listagemProdutos = async () => {
+  const capturar = document.querySelector('.items');
+  const produtos = await fetchProducts('computador');
+
+  produtos.results.forEach((ell) => {
+    const cadaProduto = createProductItemElement(ell);
+    capturar.appendChild(cadaProduto);
+  });
+};
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+console.log(getSkuFromProductItem());
 
 const cartItemClickListener = (event) => {
   // coloque seu código aqui
@@ -37,18 +47,18 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   li.addEventListener('click', cartItemClickListener);
   return li;
 };
+console.log(createCartItemElement());
 
 function aplicaLoading() {
-    const criaSection = document.createElement('section');
-    criaSection.textContent = 'Carregando...';
-    criaSection.className = 'loading';
-    document.querySelector('.items').appendChild(criaSection);
-  }
+  const criaSection = document.createElement('section');
+  criaSection.textContent = 'Carregando...';
+  criaSection.className = 'loading';
+  document.querySelector('.items').appendChild(criaSection);
+}
 function encerraLoading() {
-    document.querySelector('.loading').remove();
-   }
+  document.querySelector('.loading').remove();
+}
 
 window.onload = async () => {
-  aplicaLoading();
-  encerraLoading();
+   listagemProdutos();
 };
