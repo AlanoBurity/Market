a76739596c3bb75033880644c2ad2310b7d2e0ff
Alano <alanosantosdev@gmail.com>
2022-06-06 22:37:38 -0300

    requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -3,7 +3,7 @@ const saveCartItems = require('../helpers/saveCartItems');
 
 localStorageSimulator('setItem');
 
-describe('3 - Teste a função saveCartItems', () => {
+describe('6 - Teste a função saveCartItems', () => {
     it('Deve verificar se ao ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
       saveCartItems('<ol><li>Item</li></ol>');
       expect(localStorage.setItem).toBeCalled();
